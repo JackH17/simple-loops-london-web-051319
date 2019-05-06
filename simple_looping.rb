ef75fdc5d164f2b6aa25772288cb9eb6fb073bbe
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 13:38:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,18 +7,17 @@ loop do puts "#{phrase}"
 count += 1 
 
 break if count == number_of_times
-phrase = "Welcome to Flatiron School's Web Development Course!"
-number_of_times.times do 
-  puts "#{phrase}
-  "
+end 
+
 end
 
 
 def times_iterator(number_of_times)
   
+phrase = "Welcome to Flatiron School's Web Development Course!"
 
-    
-    puts "#{phrase}"
+number_of_times.times do 
+  puts "#{phrase}"
   end
 
   
