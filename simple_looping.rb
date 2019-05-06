c5e0dd829d8bf7edd71cd85e35fb36a33a08dfbb
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 13:30:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,6 +4,15 @@ def loop_iterator(number_of_times)
 phrase = "Welcome to Flatiron School's Web Development Course!"
 number_of_times = 0 
 
+loop do 
+  puts "#{phrase}"
+  number_of_times += 1 
+  if 
+    number_of_times > 7 break
+  end 
+  
+end
+
 end 
 
 
