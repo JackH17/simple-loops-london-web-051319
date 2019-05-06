442748411f38b29fef8598702a47a665b34424dc
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 13:41:45 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,8 +35,13 @@ end
 end
 
 def until_iterator(number_of_times)
- 
-
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  count = 0 
+  
+  until count == number_of_times
+  
+  count += 1 
+end 
 end
 
 def for_iterator(number_of_times)
