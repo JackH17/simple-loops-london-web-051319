1d2dd69c92324ca3097ad9af85f331305dc01095
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 13:27:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,33 +14,18 @@ end
 
 
 def times_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  loop 7.times do 
-    puts "#{phrase}"
-  end 
+
   
 end
 
 def while_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = 0 
-  
-  while number_of_times<8 
-  puts "#{phrase}"
-  number_of_times += 
-end 
+
   
 end
 
 def until_iterator(number_of_times)
  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = 0 
-  
-  until number_of_times > 7 
-  puts "#{phrase}"
-  number_of_times += 
-end 
+
 end
 
 def for_iterator(number_of_times)
